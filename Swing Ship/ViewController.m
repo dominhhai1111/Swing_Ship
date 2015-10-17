ae4e761bf56ef35486c44c193bab061cebd339c0
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-10-17 17:07:28 +0700

    first commit

--- Swing Ship/ViewController.m
+++ Swing Ship/ViewController.m
@@ -7,21 +7,81 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
-
+{
+    UIImageView* ship;
+    UIImageView* sea1, *sea2;
+    AVAudioPlayer* audioPlayer;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self drawShipAndSea];
+    [self animateShip];
+    [self animateSea];
+    [self playSong];
+}
+-(void) drawShipAndSea
+{
+    sea1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"sea1"]];
+    sea1.frame = self.view.bounds;
+    [self.view addSubview:sea1];
+    
+    sea2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"sea2"]];
+    sea2.frame = CGRectMake(self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+    [self.view addSubview:sea2];
+    
+    ship = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Ship2"]];
+    ship.center = CGPointMake(200, 500);
+    [self.view addSubview:ship];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) animateShip
+{
+    [UIView animateWithDuration:1 animations:^{
+        ship.transform = CGAffineTransformMakeRotation(-0.08);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1
+                         animations:^{
+                             ship.transform = CGAffineTransformMakeRotation(0.08);
+                         } completion:^(BOOL finished) {
+                             [self animateShip];
+                         }];
+    }];}
+-(void) animateSea
+{
+    [UIView animateWithDuration:10
+                     animations:^{
+                         sea1.frame = CGRectMake(-self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+                         sea2.frame = self.view.bounds;
+                     } completion:^(BOOL finished) {
+                         sea1.frame = CGRectMake(self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+                         [UIView animateWithDuration:10 animations:^{
+                             sea1.frame = self.view.bounds;
+                             sea2.frame = CGRectMake(-self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+                         } completion:^(BOOL finished) {
+                             sea2.frame = CGRectMake(self.view.bounds.size.width, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+                             [self animateSea];
+                         }];
+                         
+                     }];
 }
-
+- (void) playSong {
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"Caribe" ofType:@"mp3"];
+    NSURL *url = [NSURL fileURLWithPath:filePath];
+    NSError *error;
+    audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url
+                                                         error:&error];
+    [audioPlayer prepareToPlay];
+    [audioPlayer play];
+}
+- (void) viewWillDisappear:(BOOL)animated {
+    [audioPlayer stop];
+}
 @end
+
